1f16f4f2b595c43c7302f0d8f5e4d2cab7087e57
Zihan Liu <updogliu@updogliu-macbookpro2.roam.corp.google.com>
2017-04-13 17:18:59 -0700

    update jspbTestOptimized.js

--- tests/simple1/jspbTestOptimized.js
+++ tests/simple1/jspbTestOptimized.js
@@ -15,7 +15,7 @@
  * }
  */
 
-function l(a) {
+function aa(a) {
   var b = typeof a;
   if ("object" == b)
     if (a) {
@@ -34,8 +34,8 @@ function l(a) {
   return b
 }
 
-function m(a) {
-  var b = p;
+function l(a) {
+  var b = m;
 
   function c() {}
   c.prototype = b.prototype;
@@ -48,30 +48,30 @@ function m(a) {
     return b.prototype[c].apply(a, e)
   }
 };
-var q = null,
-  r = null;
+var p = null,
+  q = null;
 
-function aa(a) {
+function ba(a) {
   var b = new Uint8Array(Math.ceil(3 * a.length / 4)),
     c = 0;
-  ba(a, function(a) {
+  ca(a, function(a) {
     b[c++] = a
   });
   return b.subarray(0, c)
 }
 
-function ba(a, b) {
-  function c(b) {
+function ca(a, b) {
+  function c(c) {
     for (; e < a.length;) {
-      var c = a.charAt(e++),
-        d = r[c];
+      var b = a.charAt(e++),
+        d = q[b];
       if (null != d) return d;
-      if (!/^[\s\xa0]*$/.test(c)) throw Error(
-        "Unknown base64 encoding at char: " + c);
+      if (!/^[\s\xa0]*$/.test(b)) throw Error(
+        "Unknown base64 encoding at char: " + b);
     }
-    return b
+    return c
   }
-  t();
+  r();
   for (var e = 0;;) {
     var d = c(-1),
       f = c(0),
@@ -83,97 +83,24 @@ function ba(a, b) {
   }
 }
 
-function t() {
-  if (!q) {
+function r() {
+  if (!p) {
+    p = {};
     q = {};
-    r = {};
-    for (var a = 0; 65 > a; a++) q[a] =
+    for (var a = 0; 65 > a; a++) p[a] =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".charAt(
-        a), r[q[a]] = a, 62 <= a && (r[
+        a), q[p[a]] = a, 62 <= a && (q[
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.".charAt(
           a)] = a)
   }
 };
+var da = JSON.parse,
+  ea = JSON.stringify;
 
-function ca(a) {
-  return eval("(" + a + ")")
-}
+function m() {}
+var t = "function" == typeof Uint8Array;
 
-function da() {
-  this.a = u
-}
-
-function w(a, b, c) {
-  if (null == b) c.push("null");
-  else {
-    if ("object" == typeof b) {
-      if ("array" == l(b)) {
-        var e = b;
-        b = e.length;
-        c.push("[");
-        for (var d = "", f = 0; f < b; f++) c.push(d), d = e[f], w(a, a.a ? a.a
-          .call(e, String(f), d) : d, c), d = ",";
-        c.push("]");
-        return
-      }
-      if (b instanceof String || b instanceof Number || b instanceof Boolean) b =
-        b.valueOf();
-      else {
-        c.push("{");
-        f = "";
-        for (e in b) Object.prototype.hasOwnProperty.call(b, e) && (d = b[e],
-          "function" != typeof d && (c.push(f), x(e, c), c.push(":"), w(a, a.a ?
-            a.a.call(b, e, d) : d, c), f = ","));
-        c.push("}");
-        return
-      }
-    }
-    switch (typeof b) {
-      case "string":
-        x(b,
-          c);
-        break;
-      case "number":
-        c.push(isFinite(b) && !isNaN(b) ? String(b) : "null");
-        break;
-      case "boolean":
-        c.push(String(b));
-        break;
-      case "function":
-        c.push("null");
-        break;
-      default:
-        throw Error("Unknown type: " + typeof b);
-    }
-  }
-}
-var y = {
-    '"': '\\"',
-    "\\": "\\\\",
-    "/": "\\/",
-    "\b": "\\b",
-    "\f": "\\f",
-    "\n": "\\n",
-    "\r": "\\r",
-    "\t": "\\t",
-    "\x0B": "\\u000b"
-  },
-  ea = /\uffff/.test("\uffff") ? /[\\\"\x00-\x1f\x7f-\uffff]/g :
-  /[\\\"\x00-\x1f\x7f-\xff]/g;
-
-function x(a, b) {
-  b.push('"', a.replace(ea, function(a) {
-    var b = y[a];
-    b || (b = "\\u" + (a.charCodeAt(0) | 65536).toString(16).substr(1), y[
-      a] = b);
-    return b
-  }), '"')
-};
-
-function p() {}
-var z = "function" == typeof Uint8Array;
-
-function A(a, b, c) {
+function u(a, b, c) {
   a.b = null;
   b || (b = []);
   a.j = void 0;
@@ -183,7 +110,7 @@ function A(a, b, c) {
     if (a.a.length) {
       b = a.a.length - 1;
       var e = a.a[b];
-      if (e && "object" == typeof e && "array" != l(e) && !(z && e instanceof Uint8Array)) {
+      if (e && "object" == typeof e && "array" != aa(e) && !(t && e instanceof Uint8Array)) {
         a.g = b - a.f;
         a.c = e;
         break a
@@ -194,50 +121,50 @@ function A(a, b, c) {
   a.i = {};
   if (c)
     for (b = 0; b < c.length; b++) e = c[b], e < a.g ? (e += a.f, a.a[e] = a.a[
-      e] || B) : a.c[e] = a.c[e] || B
+      e] || w) : a.c[e] = a.c[e] || w
 }
-var B = [];
+var w = [];
 
-function C(a, b) {
+function x(a, b) {
   if (b < a.g) {
     b += a.f;
     var c = a.a[b];
-    return c === B ? a.a[b] = [] : c
+    return c === w ? a.a[b] = [] : c
   }
   c = a.c[b];
-  return c === B ? a.c[b] = [] : c
+  return c === w ? a.c[b] = [] : c
 }
 
-function D(a, b, c) {
-  a = C(a, b);
+function y(a, b, c) {
+  a = x(a, b);
   return null == a ? c : a
 }
 
-function E(a, b, c) {
+function z(a, b, c) {
   b < a.g ? a.a[b + a.f] = c : a.c[b] = c
 }
 
-function F(a, b, c) {
+function A(a, b, c) {
   a.b || (a.b = {});
   if (!a.b[c]) {
-    var e = C(a, c);
+    var e = x(a, c);
     e && (a.b[c] = new b(e))
   }
   return a.b[c]
 }
 
-function G(a, b, c) {
+function B(a, b, c) {
   a.b || (a.b = {});
   var e = c ? c.a : c;
   a.b[b] = c;
-  E(a, b, e)
+  z(a, b, e)
 }
-var H = this.JSON && this.JSON.stringify || "object" === typeof JSON && JSON.stringify;
-p.prototype.h = z ? function() {
+var C = JSON && JSON.stringify || "object" === typeof JSON && JSON.stringify;
+m.prototype.h = t ? function() {
   var a = Uint8Array.prototype.toJSON;
   Uint8Array.prototype.toJSON = function() {
-    t();
-    for (var a = q, b = [], d = 0; d < this.length; d += 3) {
+    r();
+    for (var a = p, b = [], d = 0; d < this.length; d += 3) {
       var f = this[d],
         g = d + 1 < this.length,
         h = g ? this[d + 1] : 0,
@@ -253,20 +180,18 @@ p.prototype.h = z ? function() {
     return b.join("")
   };
   try {
-    var b = H.call(null, this.a, u)
+    var b = C.call(null, this.a, D)
   } finally {
     Uint8Array.prototype.toJSON = a
   }
   return b
-} : H ? function() {
-  return H.call(null, this.a, u)
+} : C ? function() {
+  return C.call(null, this.a, D)
 } : function() {
-  var a = [];
-  w(new da, this.a, a);
-  return a.join("")
+  return ea(this.a, D)
 };
 
-function u(a, b) {
+function D(a, b) {
   if ("number" == typeof b) {
     if (isNaN(b)) return "NaN";
     if (Infinity === b) return "Infinity";
@@ -274,30 +199,30 @@ function u(a, b) {
   }
   return b
 }
-p.prototype.toString = function() {
+m.prototype.toString = function() {
   return this.a.toString()
 };
-var I = 0,
-  J = 0;
+var E = 0,
+  F = 0;
 
-function K(a, b, c) {
+function G(a, b, c) {
   this.b = null;
   this.g = this.h = this.a = this.f = this.i = 0;
   this.j = !1;
-  a && L(this, a, b, c)
+  a && H(this, a, b, c)
 }
-var M = [];
+var I = [];
 
-function L(a, b, c, e) {
+function H(a, b, c, e) {
   b = b.constructor === Uint8Array ? b : b.constructor === ArrayBuffer ? new Uint8Array(
       b) : b.constructor === Array ? new Uint8Array(b) : b.constructor ===
-    String ? aa(b) : new Uint8Array(0);
+    String ? ba(b) : new Uint8Array(0);
   a.b = b;
   a.i = void 0 !== c ? c : 0;
   a.f = void 0 !== e ? a.i + e : a.b.length;
   a.a = a.i
 }
-K.prototype.c = function() {
+G.prototype.c = function() {
   var a, b = this.b;
   a = b[this.a + 0];
   var c = a & 127;
@@ -317,9 +242,9 @@ K.prototype.c = function() {
   this.a += 10;
   return c
 };
-K.prototype.m = K.prototype.c;
+G.prototype.m = G.prototype.c;
 
-function N(a) {
+function J(a) {
   a: {
     for (var b, c = 0, e, d = 0; 4 > d; d++)
       if (b = a.b[a.a++], c |= (b & 127) << 7 * d, 128 > b) {
@@ -347,7 +272,7 @@ function N(a) {
   return a ? -b : b
 }
 
-function O(a) {
+function K(a) {
   var b = a.b[a.a + 0],
     c = a.b[a.a + 1],
     e = a.b[a.a + 2],
@@ -355,55 +280,55 @@ function O(a) {
   a.a += 4;
   return (b << 0 | c << 8 | e << 16 | d << 24) >>> 0
 }
-K.prototype.l = function() {
+G.prototype.l = function() {
   return !!this.b[this.a++]
 };
 
-function fa() {
+function L() {
   this.a = []
 }
-fa.prototype.length = function() {
+L.prototype.length = function() {
   return this.a.length
 };
 
-function ga(a) {
+function M(a) {
   var b = a.a;
   a.a = [];
   return b
 }
 
-function P(a, b) {
+function N(a, b) {
   for (; 127 < b;) a.a.push(b & 127 | 128), b >>>= 7;
   a.a.push(b)
 }
 
-function ha(a, b) {
-  if (0 <= b) P(a, b);
+function O(a, b) {
+  if (0 <= b) N(a, b);
   else {
     for (var c = 0; 9 > c; c++) a.a.push(b & 127 | 128), b >>= 7;
     a.a.push(1)
   }
 }
 
-function Q(a, b) {
+function P(a, b) {
   a.a.push(b >>> 0 & 255);
   a.a.push(b >>> 8 & 255);
   a.a.push(b >>> 16 & 255);
   a.a.push(b >>> 24 & 255)
 };
 
-function ia(a) {
-  if (M.length) {
-    var b = M.pop();
-    a && L(b, a, void 0, void 0);
+function fa(a) {
+  if (I.length) {
+    var b = I.pop();
+    a && H(b, a, void 0, void 0);
     a = b
-  } else a = new K(a, void 0, void 0);
+  } else a = new G(a, void 0, void 0);
   this.a = a;
   this.b = this.c = -1;
   this.f = !1
 }
 
-function R(a) {
+function Q(a) {
   var b = a.a;
   (b = b.a == b.f) || (b = a.f) || (b = a.a, b = b.j || 0 > b.a || b.a > b.f);
   if (b) return !1;
@@ -416,20 +341,20 @@ function R(a) {
   return !0
 }
 
-function S(a) {
+function R(a) {
   switch (a.b) {
     case 0:
-      if (0 != a.b) S(a);
+      if (0 != a.b) R(a);
       else {
         for (a = a.a; a.b[a.a] & 128;) a.a++;
         a.a++
       }
       break;
     case 1:
-      1 != a.b ? S(a) : (a = a.a, a.a += 8);
+      1 != a.b ? R(a) : (a = a.a, a.a += 8);
       break;
     case 2:
-      if (2 != a.b) S(a);
+      if (2 != a.b) R(a);
       else {
         var b = a.a.c();
         a = a.a;
@@ -437,12 +362,12 @@ function S(a) {
       }
       break;
     case 5:
-      5 != a.b ? S(a) : (a = a.a, a.a += 4);
+      5 != a.b ? R(a) : (a = a.a, a.a += 4);
       break;
     case 3:
       b = [a.c];
       do {
-        if (!R(a)) {
+        if (!Q(a)) {
           a.f = !0;
           break
         }
@@ -455,7 +380,7 @@ function S(a) {
   }
 }
 
-function T(a, b, c) {
+function S(a, b, c) {
   var e = a.a.f,
     d = a.a.c(),
     d = a.a.a + d;
@@ -465,22 +390,22 @@ function T(a, b, c) {
   a.a.f = e
 };
 
-function ja() {
+function ga() {
   this.c = [];
   this.b = 0;
-  this.a = new fa
+  this.a = new L
 }
 
-function ka(a, b) {
+function T(a, b) {
   U(a, b, 2);
-  b = ga(a.a);
+  b = M(a.a);
   a.c.push(b);
   a.b += b.length;
   b.push(a.b);
   return b
 }
 
-function la(a, b) {
+function ha(a, b) {
   for (var c = b.pop(), c = a.b + a.a.length() - c; 127 < c;) b.push(c & 127 |
     128), c >>>= 7, a.b++;
   b.push(c);
@@ -488,81 +413,81 @@ function la(a, b) {
 }
 
 function U(a, b, c) {
-  P(a.a, 8 * b + c)
+  N(a.a, 8 * b + c)
 }
 
 function V(a, b, c, e) {
-  null != c && (b = ka(a, b), e(c, a), la(a, b))
+  null != c && (b = T(a, b), e(c, a), ha(a, b))
 };
 
 function W(a) {
-  A(this, a, null)
+  u(this, a, null)
 }
-m(W);
+l(W);
 
 function X(a) {
-  A(this, a, null)
+  u(this, a, null)
 }
-m(X);
+l(X);
 
-function ma(a, b) {
-  for (; R(b) && 4 != b.b;) switch (b.c) {
+function ia(a, b) {
+  for (; Q(b) && 4 != b.b;) switch (b.c) {
     case 1:
       var c = b.a.m();
-      E(a, 1, c);
+      z(a, 1, c);
       break;
     case 2:
       c = new Y;
-      T(b, c, na);
-      G(a, 2, c);
+      S(b, c, ja);
+      B(a, 2, c);
       break;
     case 3:
       c = new Z;
-      T(b, c, oa);
-      G(a, 3, c);
+      S(b, c, ka);
+      B(a, 3, c);
       break;
     default:
-      S(b)
+      R(b)
   }
   return a
 }
 
-function pa(a, b) {
+function la(a, b) {
   var c;
-  (c = D(a, 1, 0)) && null != c && null != c && (U(b, 1, 0), ha(b.a, c));
-  (c = F(a, Y, 2)) && V(b, 2, c, qa);
-  (c = F(a, Z, 3)) && V(b, 3, c, ra)
+  (c = y(a, 1, 0)) && null != c && null != c && (U(b, 1, 0), O(b.a, c));
+  (c = A(a, Y, 2)) && V(b, 2, c, ma);
+  (c = A(a, Z, 3)) && V(b, 3, c, na)
 }
 
 function Y(a) {
-  A(this, a, null)
+  u(this, a, null)
 }
-m(Y);
+l(Y);
 
-function na(a, b) {
-  for (; R(b) && 4 != b.b;) switch (b.c) {
+function ja(a, b) {
+  for (; Q(b) && 4 != b.b;) switch (b.c) {
     case 1:
-      var c = N(b.a);
-      E(a, 1, c);
+      var c = J(b.a);
+      z(a, 1, c);
       break;
     case 2:
-      c = N(b.a);
-      E(a, 2, c);
+      c = J(b.a);
+      z(a, 2, c);
       break;
     case 3:
       c = b.a.c();
       c = c >>> 1 ^ -(c & 1);
-      E(a, 3, c);
+      z(a, 3, c);
       break;
     default:
-      S(b)
+      R(b)
   }
   return a
 }
 
-function qa(a, b) {
+function ma(a, b) {
   var c;
-  if ((c = D(a, 1, 0)) && null != c) {
+  if ((c = y(a, 1, 0)) && null != c) {
     var e = c;
     if (null != e) {
       U(b, 1, 0);
@@ -575,73 +500,73 @@ function qa(a, b) {
         d = d >>> 0;
       e && (d = ~d >>> 0, f = (~f >>> 0) + 1, 4294967295 < f && (f = 0, d++,
         4294967295 < d && (d = 0)));
-      I = f;
-      J = d;
-      e = I;
-      for (f = J; 0 < f || 127 < e;) c.a.push(e & 127 | 128), e = (e >>> 7 | f <<
+      E = f;
+      F = d;
+      e = E;
+      for (f = F; 0 < f || 127 < e;) c.a.push(e & 127 | 128), e = (e >>> 7 | f <<
         25) >>> 0, f >>>= 7;
       c.a.push(e)
     }
   }
-  c = D(a, 2, 0);
-  0 !== c && null != c && (U(b, 2, 0), ha(b.a, c));
-  (c = D(a, 3, 0)) && null != c && (a = c, null != a && (U(b, 3, 0), P(b.a, (a <<
+  c = y(a, 2, 0);
+  0 !== c && null != c && (U(b, 2, 0), O(b.a, c));
+  (c = y(a, 3, 0)) && null != c && (a = c, null != a && (U(b, 3, 0), N(b.a, (a <<
     1 ^ a >> 31) >>> 0)))
 }
 
 function Z(a) {
-  A(this, a, sa)
+  u(this, a, oa)
 }
-m(Z);
-var sa = [1];
+l(Z);
+var oa = [1];
 
-function oa(a, b) {
-  for (; R(b) && 4 != b.b;) switch (b.c) {
+function ka(a, b) {
+  for (; Q(b) && 4 != b.b;) switch (b.c) {
     case 1:
       var c;
       c = b;
       for (var e = b.a.l, d = c.a.c(), d = c.a.a + d, f = []; c.a.a < d;) f.push(
         e.call(c.a));
       c = f;
-      E(a, 1, c || []);
+      z(a, 1, c || []);
       break;
     case 2:
       e = b.a;
-      c = O(e);
-      f = O(e);
+      c = K(e);
+      f = K(e);
       e = 2 * (f >> 31) + 1;
       d = f >>> 20 & 2047;
       c = 4294967296 * (f & 1048575) + c;
       c = 2047 == d ? c ? NaN : Infinity * e : d ? e * Math.pow(2, d - 1075) *
         (c + 4503599627370496) : e * Math.pow(2, -1074) * c;
-      E(a, 2, c);
+      z(a, 2, c);
       break;
     default:
-      S(b)
+      R(b)
   }
   return a
 }
 
-function ra(a, b) {
+function na(a, b) {
   var c;
-  c = C(a, 1);
+  c = x(a, 1);
   if (0 < c.length && c && c.length) {
     U(b, 1, 2);
-    P(b.a, c.length);
+    N(b.a, c.length);
     for (var e = 0; e < c.length; e++) b.a.a.push(c[e] ? 1 : 0)
   }
-  if (c = +D(a, 2, 0)) a = c, null != a && (U(b, 2, 1), b = b.a, e = a, (e = (a =
-      0 > e ? 1 : 0) ? -e : e) ? isNaN(e) ? (J = 2147483647, I = 4294967295) :
-    1.7976931348623157E308 < e ? (J = (a << 31 | 2146435072) >>> 0, I = 0) :
-    2.2250738585072014E-308 > e ? (e /= Math.pow(2, -1074), J = (a << 31 | e /
-      4294967296) >>> 0, I = e >>> 0) : (c = Math.floor(Math.log(e) / Math.LN2),
-      1024 == c && (c = 1023), e *= Math.pow(2, -c), J = (a << 31 | c + 1023 <<
-        20 | 1048576 * e & 1048575) >>> 0, I = 4503599627370496 *
-      e >>> 0) : (J = 0 < 1 / e ? 0 : 2147483648, I = 0), Q(b, I), Q(b, J))
+  if (c = +y(a, 2, 0)) a = c, null != a && (U(b, 2, 1), b = b.a, e = a, (e = (a =
+      0 > e ? 1 : 0) ? -e : e) ? isNaN(e) ? (F = 2147483647, E = 4294967295) :
+    1.7976931348623157E308 < e ? (F = (a << 31 | 2146435072) >>> 0, E = 0) :
+    2.2250738585072014E-308 > e ? (e /= Math.pow(2, -1074), F = (a << 31 | e /
+      4294967296) >>> 0, E = e >>> 0) : (c = Math.floor(Math.log(e) / Math.LN2),
+      1024 == c && (c = 1023), e *= Math.pow(2, -c), F = (a << 31 | c + 1023 <<
+        20 | 1048576 * e & 1048575) >>> 0, E = 4503599627370496 *
+      e >>> 0) : (F = 0 < 1 / e ? 0 : 2147483648, E = 0), P(b, E), P(b, F))
 };
 module.exports.deserializeBinaryTest = function(a) {
-  a = new ia(a);
-  for (var b = new W; R(a) && 4 != a.b;) switch (a.c) {
+  a = new fa(a);
+  for (var b = new W; Q(a) && 4 != a.b;) switch (a.c) {
     case 1:
       var c;
       c = a;
@@ -661,8 +586,8 @@ module.exports.deserializeBinaryTest = function(a) {
         } else if (248 > k) {
           var n = d[f++],
             v = d[f++],
-            ta = d[f++],
-            k = (k & 7) << 18 | (n & 63) << 12 | (v & 63) << 6 | ta & 63,
+            pa = d[f++],
+            k = (k & 7) << 18 | (n & 63) << 12 | (v & 63) << 6 | pa & 63,
             k = k - 65536;
           g.push((k >> 10 & 1023) + 55296, (k & 1023) + 56320)
         }
@@ -672,37 +597,37 @@ module.exports.deserializeBinaryTest = function(a) {
       h += String.fromCharCode.apply(null, g);
       c.a = f;
       c = h;
-      E(b, 1, c);
+      z(b, 1, c);
       break;
     case 2:
       c = a.a.c();
-      E(b, 2, c);
+      z(b, 2, c);
       break;
     case 3:
       c = new X;
-      T(a, c, ma);
-      G(b, 3, c);
+      S(a, c, ia);
+      B(b, 3, c);
       break;
     case 4:
-      f = O(a.a);
+      f = K(a.a);
       c = 2 * (f >> 31) + 1;
       d = f >>> 23 & 255;
       f &= 8388607;
       c = 255 == d ? f ? NaN : Infinity * c : d ? c * Math.pow(2, d - 150) *
         (f + Math.pow(2, 23)) : c * Math.pow(2, -149) * f;
-      E(b, 4, c);
+      z(b, 4, c);
       break;
     default:
-      S(a)
+      R(a)
   }
   return b
 };
 module.exports.serializeBinaryTest = function(a) {
-  var b = new ja,
+  var b = new ga,
     c;
-  c = D(a, 1, "");
+  c = y(a, 1, "");
   if (0 < c.length && null != c) {
-    for (var e = ka(b, 1), d = b.a, f = 0; f < c.length; f++) {
+    for (var e = T(b, 1), d = b.a, f = 0; f < c.length; f++) {
       var g = c.charCodeAt(f);
       if (128 > g) d.a.push(g);
       else if (2048 > g) d.a.push(g >> 6 | 192), d.a.push(g & 63 | 128);
@@ -716,28 +641,28 @@ module.exports.serializeBinaryTest = function(a) {
           g &
           63 | 128)
     }
-    la(b, e)
-  }(c = D(a, 2, 0)) && null != c && null != c && (U(b, 2, 0), P(b.a, c));
-  (c = F(a, X, 3)) && V(b, 3, c, pa);
-  (c = +D(a, 4, 0)) && null != c && (U(b, 4, 5), a = b.a, d = c, (d = (c = 0 >
-      d ? 1 : 0) ? -d : d) ? isNaN(d) ? (J = 0, I = 2147483647) :
-    3.4028234663852886E38 < d ? (J = 0, I = (c << 31 | 2139095040) >>> 0) :
-    1.1754943508222875E-38 > d ? (d = Math.round(d / Math.pow(2, -149)), J =
-      0, I = (c << 31 | d) >>> 0) : (e = Math.floor(Math.log(d) / Math.LN2),
-      d *= Math.pow(2, -e), d = Math.round(8388608 * d) & 8388607, J = 0, I =
-      (c << 31 | e + 127 << 23 | d) >>> 0) : 0 < 1 / d ? I = J = 0 : (J = 0,
-      I = 2147483648), Q(a, I));
+    ha(b, e)
+  }(c = y(a, 2, 0)) && null != c && null != c && (U(b, 2, 0), N(b.a, c));
+  (c = A(a, X, 3)) && V(b, 3, c, la);
+  (c = +y(a, 4, 0)) && null != c && (U(b, 4, 5), a = b.a, d = c, (d = (c = 0 >
+      d ? 1 : 0) ? -d : d) ? isNaN(d) ? (F = 0, E = 2147483647) :
+    3.4028234663852886E38 < d ? (F = 0, E = (c << 31 | 2139095040) >>> 0) :
+    1.1754943508222875E-38 > d ? (d = Math.round(d / Math.pow(2, -149)), F =
+      0, E = (c << 31 | d) >>> 0) : (e = Math.floor(Math.log(d) / Math.LN2),
+      d *= Math.pow(2, -e), d = Math.round(8388608 * d) & 8388607, F = 0, E =
+      (c << 31 | e + 127 << 23 | d) >>> 0) : 0 < 1 / d ? E = F = 0 : (F = 0,
+      E = 2147483648), P(a, E));
   a = new Uint8Array(b.b + b.a.length());
   e = b.c;
   d = e.length;
   for (f = c = 0; f < d; f++) g = e[f], a.set(g, c), c += g.length;
-  e = ga(b.a);
+  e = M(b.a);
   a.set(e, c);
   b.c = [a];
   return a
 };
 module.exports.deserializeTextTest = function(a) {
-  return new W(ca(a))
+  return new W(da(a))
 };
 module.exports.serializeTextTest = function(a) {
   return a.h()
